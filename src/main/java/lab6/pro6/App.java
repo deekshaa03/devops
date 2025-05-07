031a2168f70f5e6874b161be097f90f3537dbb15
deekshaa03 <deekshakotian928@gmail.com>
2025-05-07 09:32:48 +0530

    initial commit

---
+++ src/main/java/lab6/pro6/App.java
@@ -0,0 +1,13 @@
+package lab6.pro6;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
